185e4e54ee098bd28e1578b402dce298b11fa687
naveendk <naveenkolambage@gmail.com>
2019-03-25 05:58:54 +1200

    before adding navbar counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value
-  };
+  // state = {
+  //   value: this.props.counter.value
+  // };
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
-    console.log("props", this.props);
+    //console.log("props", this.props);
 
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           INCREMENT
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn btn-danger btn-sm m-2"
         >
           Delete
@@ -33,12 +33,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
